c1075bbf884e6650d3349d212bcc8ebb8b7f13ba
BorisP <borisp@hiroshima.local>
2018-01-29 18:38:37 +0100

    viewport: - my test to show how to use juce gui to create viewport with oversized layout component, where you can drag components and autoscroll the layout once the component reaches window edge. works pretty nice.

---
+++ viewport/Source/MainComponent.cpp
@@ -0,0 +1,57 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#include "MainComponent.h"
+
+using namespace std;
+
+//==============================================================================
+
+void NodeComponent::mouseDrag(const juce::MouseEvent &e)
+{
+    dragger.dragComponent (this, e, &constrainer);
+    LayoutComponent *layout = dynamic_cast<LayoutComponent *>(getParentComponent());
+    assert(layout);
+    MouseEvent re (e.getEventRelativeTo(layout->getMain()));
+    layout->getMain()->getViewport().autoScroll(re.x, re.y, 10,8);
+}
+
+MainContentComponent::MainContentComponent()
+{
+    setSize (600, 400);
+    view.setViewedComponent(&layout, false);
+    addAndMakeVisible(view);
+    layout.setMain(this);
+}
+
+MainContentComponent::~MainContentComponent()
+{
+}
+
+void MainContentComponent::paint (Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+
+    g.setFont (Font (16.0f));
+    g.setColour (Colours::white);
+    g.drawText ("Hello World!", getLocalBounds(), Justification::centred, true);
+}
+
+void MainContentComponent::resized()
+{
+    // This is called when the MainContentComponent is resized.
+    // If you add any child components, this is where you should
+    // update their positions.
+    
+    auto r = getBounds();
+    r.reduce(10, 10);
+    view.setBounds(r);
+    r = getBounds();
+    layout.setBounds(0,0,1000,1000);
+}
---
+++ viewport/Source/MainComponent.h
@@ -0,0 +1,139 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+using namespace std;
+class MainContentComponent;
+
+class NodeComponent : public Component
+{
+public:
+    NodeComponent()
+    {
+        
+    }
+    
+    ~NodeComponent()
+    {
+        
+    }
+
+    void paint (Graphics& g)
+    {
+        // (Our component is opaque, so we must completely fill the background with a solid colour)
+        g.fillAll (Colours::slategrey);
+        
+        g.setFont (Font (16.0f));
+        g.setColour (Colours::white);
+        g.drawText ("Node", getLocalBounds(), Justification::centred, true);
+    }
+    
+    void resized()
+    {
+        // This is called when the MainContentComponent is resized.
+        // If you add any child components, this is where you should
+        // update their positions.
+    }
+    
+    void mouseDown (const MouseEvent& e)
+    {
+        dragger.startDraggingComponent (this, e);
+    }
+    
+    void mouseUp (const MouseEvent& e)
+    {
+        
+    }
+    
+    void mouseDrag (const MouseEvent& e) override;
+
+private:
+    ComponentDragger dragger;
+    ComponentBoundsConstrainer constrainer;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (NodeComponent)
+};
+
+class LayoutComponent : public Component
+{
+public:
+    LayoutComponent()
+    {
+        addAndMakeVisible(&node);
+        cout << "Layout added" << endl;
+    }
+    
+    ~LayoutComponent()
+    {
+        cout << "Layout destroyed" << endl;
+    }
+    
+    void setMain(MainContentComponent *_main)
+    {
+        main = _main;
+    }
+    
+    MainContentComponent *getMain()
+    {
+        return main;
+    }
+    
+    void paint (Graphics& g)
+    {
+        // (Our component is opaque, so we must completely fill the background with a solid colour)
+        g.fillAll (Colours::blueviolet);
+        
+        g.setFont (Font (16.0f));
+        g.setColour (Colours::white);
+        g.drawText ("Layout", getLocalBounds(), Justification::centred, true);
+    }
+    
+    void resized()
+    {
+        // This is called when the MainContentComponent is resized.
+        // If you add any child components, this is where you should
+        // update their positions.
+        Rectangle<int> r(0,0,100,100);
+        node.setBounds(r);
+    }
+    
+private:
+    MainContentComponent *main;
+    NodeComponent node;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (LayoutComponent)
+};
+
+//==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
+class MainContentComponent   : public Component
+{
+public:
+    //==============================================================================
+    MainContentComponent();
+    ~MainContentComponent();
+
+    void paint (Graphics&) override;
+    void resized() override;
+
+    Viewport &getViewport()
+    {
+        return view;
+    }
+
+private:
+    Viewport view;
+    LayoutComponent layout;
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainContentComponent)
+};
