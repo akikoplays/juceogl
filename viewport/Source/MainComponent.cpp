4b9150349e1ac3810ce030d3372259bf73371e03
BorisP <borisp@hiroshima.local>
2018-01-30 12:01:44 +0100

    viewport: - experiment with snap to grid constraint while dragging components

--- viewport/Source/MainComponent.cpp
+++ viewport/Source/MainComponent.cpp
@@ -12,14 +12,49 @@ using namespace std;
 
 //==============================================================================
 
+
+void SnapConstraint::applyBoundsToComponent (Component &component, Rectangle<int> bounds)
+{
+    cout << "Apply bounds" << endl;
+    if (auto* positioner = component.getPositioner())
+        positioner->applyNewBounds (bounds);
+    else {
+// original code:
+//        component.setBounds (bounds);
+        int grid = 20;
+        int x = bounds.getX() / grid * grid;
+        int y = bounds.getY() / grid * grid;
+        bounds.setX(x);
+        bounds.setY(y);
+        component.setBounds(bounds);
+    }
+}
+
+void NodeComponent::mouseDown (const MouseEvent& e)
+{
+    dragger.startDraggingComponent (this, e);\
+}
+
+void NodeComponent::mouseUp (const MouseEvent& e)
+{
+}
+
 void NodeComponent::mouseDrag(const juce::MouseEvent &e)
 {
     dragger.dragComponent (this, e, &constrainer);
+    //    int x = e.getDistanceFromDragStartX(), y = e.getDistanceFromDragStartY();
+    //    if(e.getDistanceFromDragStartX()%10==0)
+    //        x = e.getDistanceFromDragStartX();
+    //    if(e.getDistanceFromDragStartY()%10==0)
+    //        y = e.getDistanceFromDragStartY();
+    //    setTopLeftPosition(x+pos.getX(), y+pos.getY());
+    
     LayoutComponent *layout = dynamic_cast<LayoutComponent *>(getParentComponent());
     assert(layout);
     MouseEvent re (e.getEventRelativeTo(layout->getMain()));
     layout->getMain()->getViewport().autoScroll(re.x, re.y, 10,8);
 }
+
 
 MainContentComponent::MainContentComponent()
 {
